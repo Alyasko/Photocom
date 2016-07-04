29a2567988c008d77e1793002992c4470f13bd57
Alexander Yasko <yasko.alexander@gmail.com>
2016-07-04 18:43:41 +0300

    Added several classes.

---
+++ Photocom/Photocom.BLL/Entities/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photocom.BLL.Entities
+{
+    public class Photo
+    {
+        public Photo()
+        {
+            
+        }
+
+        public IEnumerable<Comment> Comments { get; set; } 
+    }
+}
